769db6c317d40f81f2f4e5a6db6f723879c26b06
Boldeusz <mnpower@poczta.onet.pl>
2021-01-17 19:05:13 +0100

    added code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(TeenNumberChecker.hasTeen(10,15,6));
+        System.out.println(TeenNumberChecker.isTeen(4));
+    }
 }
